1d46369a1c3cfe53b441a809a11a9b7c65afa664
iambstha <iambstha@gmail.com>
2023-07-04 22:53:14 +0545

    Added some style

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { decrement, increment } from './counterSlice'
-
+import './counter.css'
 const Counter = () => {
     const count = useSelector(state => state.counter.value)
     const dispatch = useDispatch()
-  return (
-    <div>
-        <p>Count: {count}</p>
-        <button onClick={() => dispatch(increment())} >Add</button>
-        <button onClick={() => dispatch(decrement())} >Subtract</button>
-    </div>
-  )
+    return (
+        <div className='box' >
+            <p className='count'><span>Count: </span>{count}</p>
+            <div className=' btn ' >
+                <button onClick={() => dispatch(increment())} >Add</button>
+                <button onClick={() => dispatch(decrement())} >Subtract</button>
+            </div>
+        </div>
+    )
 }
 
 export default Counter
